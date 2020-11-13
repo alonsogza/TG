ca1709759c98d39fff1e8cdb6a0c76796fa4cade
Alonso Garza <JuanAGC@soriana.com>
2020-11-13 10:40:20 -0600

    Actualizacion al sitio, queda pendiente agregar la clase active en el menu una vez seleccionado la opcion

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"# TG" 
+# TG
+### Proyecto Landing Page 
+
+Se utiliza los siguientes elementos:
+
+- HTML
+- CSS
+- Javascript
+- Bootstrap v4.5.2
+- Normalize v8.0.1
+- VUE v2.6.12
+
+
+---
+
+:zap: Wave: https://smooth.ie/blogs/news/svg-wavey-transitions-between-sections
+:zap: Normalize: 
+
+---
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,27 +1,50 @@
-let boton = document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
-let contador = 0;
+// let boton = document.getElementById("icono");
+// let enlaces = document.getElementById("enlaces");
+// let contador = 1;
+console.log('Estoy activo...;');
+// boton.addEventListener("click", function(e){
+//     console.log(`Click`);
+//     e.preventDefault();
+//     if(contador==0){
+//         enlaces.className = ("enlaces dos")
+//         console.log(`OP: ${contador}`)
+//         contador=1;
+//     }else{
+//         enlaces.classList.remove("dos")
+//         enlaces.className = ("enlaces uno")
+//         console.log(`OP: ${contador}`)        
+//         contador=0;
+//     }
+// })
 
-boton.addEventListener("click", function(/* e */){
-    // e.preventDefault();
-    if(contador==0){
-        enlaces.className = ("enlaces dos")
-        console.log(`OP: ${contador}`)
-        contador=1;
-    }else{
-        enlaces.classList.remove("dos")
-        enlaces.className = ("enlaces uno")
-        console.log(`OP: ${contador}`)        
-        contador=0;
+document.addEventListener(`click`, e => {
+    const origin = e.target.closest("a");
+    
+    if (origin) {
+        // debugger;
+        origin.classList.remove('active')
+        console.clear();
+        console.log(`You clicked ${origin.href}`);
+
+//         if(origin.includes('Guisos')){
+        origin.classList.add('active')
+//         }
+
+// origin.includes('Guisos')
+// origin.includes('Ubicacion')
+        
     }
-})
+  });
+
 
 const app = new Vue({
     el: '#app',
     data:{
         titulo: 'Los Tacos del Gera',
-        subtitulo: 'Exquisitos tacos, ven y pruebalos'
+        subtitulo: 'Exquisitos tacos, ven y pruebalos',
+        ubicacion: 'Nuestra Ubicación',
+        tit_guisos: 'Nuestra Especialidad',
+        tel: '81 12345678'
     }
-
 
 })
