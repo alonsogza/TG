6705af93590c15663f8b8e987239bdfa2cdd4f3b
Alonso Garza <alonso_03@hotmail.com>
2020-11-14 04:10:30 -0600

    Se incluye logica para ocultar las opciones del menu hamburguesa por medio de javascript

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,22 +3,26 @@
 const btnMenu = document.querySelector('#btn-menu')
 const a = document.getElementsByTagName('a');
 
-document.addEventListener(`click`, e => {
-    const origin = e.target.closest("a");
-    // btnMenu.click();
+document.addEventListener(`click`, (e) => {
+  const origin = e.target.closest("a");
+  const ancho = document.body.scrollWidth;
 
+  if (ancho < 751) {
+    btnMenu.click();
+  }
+
+  if (origin) {
     //Obtenenemos todos los anchors
-    for (let idx= 0; idx < a.length; ++idx){
-        // console.log(a[idx].href);
-        a[idx].classList.remove("active");
-    }    
-
-    if (origin) {
-        // console.clear();
-        // console.log(`You clicked ${origin.href}`);
-        origin.classList.add('active')
+    for (let idx = 0; idx < a.length; ++idx) {
+      // console.log(a[idx].href);
+      a[idx].classList.remove("active");
     }
-  });
+
+    // console.clear();
+    // console.log(`You clicked ${origin.href}`);
+    origin.classList.add("active");
+  }
+});
 
 
 const app = new Vue({
