2430fe631dca96700df6b88af4f4a480679b6155
Alonso Garza <alonso_03@hotmail.com>
2020-11-18 05:59:05 -0600

    Se actualiza el codigo quitando los comentarios, ademas se hace uso de VUE para crear los componentes -olainferior- y -olasuperior- dicho codigo se encuentra en -script.js-

--- README.md
+++ README.md
@@ -6,19 +6,26 @@ Una sola página donde utilizo los siguientes elementos:
 - HTML
 - CSS
 - Javascript
+- uiGradients
 - Bootstrap v4.5.2
+- jQuery v3.5.1
 - Normalize v8.0.1
 - VUE v2.6.12
 - Font Awesome 5.15.1
-
 
 ---
 
 :zap: Imagen: https://www.pexels.com
 
+:zap: TinyPNG: https://tinypng.com/
+
+:zap: uiGradients: https://uigradients.com
+
 :zap: Wave: https://smooth.ie/blogs/news/svg-wavey-transitions-between-sections
 
 :zap: Bootstrap: https://getbootstrap.com/ 
+
+:zap: jQuery: https://jquery.com/
 
 :zap: Normalize: https://necolas.github.io/normalize.css/
 
--- docs/js/script.js
+++ docs/js/script.js
@@ -7,8 +7,7 @@ document.addEventListener(`click`, (e) => {
   const origin = e.target.closest("a");
   const ancho = document.body.scrollWidth;
 
-
-  console.log(`Actualente cuenta con un ancho de ${ancho}, ${origin}`);
+  // console.log(`Actualente cuenta con un ancho de ${ancho}, ${origin}`);
   if ((ancho <= 750) && (origin.href.includes('Guisos') || (origin.href.includes('Ubicacion')))) {
       btnMenu.click();
   };
@@ -21,11 +20,39 @@ document.addEventListener(`click`, (e) => {
     }
 
     // console.clear();
-    console.log(`You clicked ${origin.href}`);
+    // console.log(`You clicked ${origin.href}`);
     origin.classList.add("active");
   }
 });
 
+
+//Para crear un COMPONENTE es: Vue.componet('')
+// El primer parametro es la ETIQUETA que asignamos, en este caso <olainferior>
+Vue.component("olainferior", {
+  template: /*html*/ `
+  <div style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+    style="height: 100%; width: 100%;">
+      <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+        style="stroke: none; fill: #fff;"></path>
+    </svg></div>
+  `,
+  data() {
+    return;
+  },
+});
+
+Vue.component("olasuperior", {
+  template: /*html*/ `
+  <div style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+    style="height: 100%; width: 100%;">
+      <path d="M0.00,49.98 C150.00,150.00 271.49,-50.00 500.00,49.98 L500.00,0.00 L0.00,0.00 Z"
+          style="stroke: none; fill: #fff;"></path>
+    </svg></div>
+  `,
+  data() {
+    return;
+  },
+});
 
 const app = new Vue({
   el: "#app",
